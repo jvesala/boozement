9f2f9aa124ab2717c08a7abad1a5b7992214160d
Jussi Vesala <jussi.vesala@iki.fi>
2010-11-24 10:59:02 +0200

    add scalatra dependency

--- project/build/Boozement.scala
+++ project/build/Boozement.scala
@@ -1,6 +1,10 @@
 
 import sbt._
 class Boozement(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
-   // ...
+  val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  val sonatypeNexusReleases = "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
+
+  val scalatra = "org.scalatra" %% "scalatra" % "2.0.0.M2"
+
 }
 
