2ef4228bd2d7e8316f07b62a935946d6976511fe
Jussi Vesala <jussi.vesala@iki.fi>
2010-12-06 01:01:57 +0200

    show current time on insert

--- src/main/webapp/js/boozement.js
+++ src/main/webapp/js/boozement.js
@@ -1,9 +1,17 @@
 function hideTabs() { $('.tab').each(function() { $(this).hide() }); }
 function deSelectTabHeader() { $('.tab-header').each(function() { $(this).removeClass("selected") }); }
 
+function getCurrentTime() {
+  var now = new Date();
+  var hours = (now.getHours() > 10) ? now.getHours() : "0"  + now.getHours();
+  var minutes = (now.getMinutes() > 10) ? now.getMinutes() : "0"  + now.getMinutes();
+  return hours + ":" + minutes;
+}
+
 function showInsertTab() {
   hideTabs();
   deSelectTabHeader();
+  $('#time').val(getCurrentTime());
   $('#tab-insert').fadeIn();
   $('.tab-header-insert').addClass("selected");
 }
