f42ffbf7035a56a0020960d2fd3966af2b232c72
Jussi Vesala <jussi.vesala@iki.fi>
2010-12-12 02:31:35 +0200

    fix time validation

--- src/main/webapp/js/validation-insert.js
+++ src/main/webapp/js/validation-insert.js
@@ -1,15 +1,12 @@
 function timeValidator() {
   return function(x) {
-    return  (x != undefined ) && x.match(/\d\d:\d\d/);
+    return ((x != undefined ) && (x.match(/\d\d:?\d\d/) != undefined)).orFailure("invalid");
   }
 }
 
 $(function() {
   var time = $('#time').changes();
   var timeValidation = mkValidation(time, timeValidator());
-
   timeValidation.Subscribe(toggleEffect($('.time-error')));
   timeValidation.Subscribe(toggleClassEffect($('#time'), 'invalid'));
-
-  timeValidation.Subscribe(function(x) { console.log("we got: " + x) });
 });
