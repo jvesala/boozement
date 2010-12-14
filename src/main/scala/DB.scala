3f9e8d4737ea864782c481086365f70ea81d962b
Jussi Vesala <jussi.vesala@iki.fi>
2010-12-14 13:28:19 +0200

    add servings table

--- src/main/scala/DB.scala
+++ src/main/scala/DB.scala
@@ -7,6 +7,7 @@ import org.scalaquery.util.NamingContext
 import org.scalaquery.ql.extended.MySQLDriver
 import org.scalaquery.ql.extended.MySQLDriver.Implicit._
 import org.scalaquery.ql.basic.{BasicTable => Table}
+import java.sql.Date
 
 object InitDatabase {
   def apply() {
@@ -14,7 +15,8 @@ object InitDatabase {
     DB.db withSession {
 	    import org.scalaquery.simple.StaticQuery.updateNA
 	    updateNA("DROP TABLE IF EXISTS users").execute
-			(Users.ddl) create
+	    updateNA("DROP TABLE IF EXISTS servings").execute
+			(Users.ddl ++ ServingsTable.ddl) create
     }
   }
 }
@@ -22,6 +24,15 @@ object InitDatabase {
 object DB {
   val db = Database.forURL("jdbc:mysql://127.0.0.1:3306/boozement?user=boozement&password=boozement", driver = "com.mysql.jdbc.Driver")
 }
+
+object ServingsTable extends Table[Servings]("servings") {
+  def id = column[Int]("id", O PrimaryKey)
+  def date = column[Date]("date")
+  def servingType = column[String]("type")
+  def amount = column[Int]("amount")
+  def * = id ~ date ~ servingType ~ amount <> (Servings, Servings.unapply _)
+}
+case class Servings(id: Int, date: Date, servingType: String, amount: Int)
 
 object Users extends Table[(Int, String, String, String)]("users") {
   def id = column[Int]("id", O PrimaryKey)
