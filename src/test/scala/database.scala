c254047de1b0ff36ccd2e0ddc3a464b275b42ea9
Jussi Vesala <jussi.vesala@iki.fi>
2010-12-19 13:23:47 +0200

    add asserts

--- src/test/scala/database.scala
+++ src/test/scala/database.scala
@@ -9,7 +9,15 @@ class DBSuite extends FunSuite with BeforeAndAfterAll {
   }
   
   test("serving is inserted into db") {    
-    val time = new DateTime(2010, 3, 26, 12, 0, 0, 0)
-    DB.insertServing(time, "Olut", 33) 
+    val drinkingTime = new DateTime(2010, 3, 26, 12, 0, 0, 0)
+    DB.insertServing(drinkingTime, "Olut", 33)
+    val servings = DB.servings
+    assert(servings.size == 1)
+    assert(servings.head.id == Some(1))
+    assert(servings.head.amount == 33)
+    
+    println("we got " + servings.head.date.getClass)
+    assert(servings.head.date == drinkingTime)
+    
   }
 }
