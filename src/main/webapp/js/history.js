41d17e439aac65e767f363a1e0f11b347eabe3e0
Jussi Vesala <jussi.vesala@iki.fi>
2010-12-24 13:39:37 +0200

    show servings in history tab

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -1,3 +1,16 @@
-$(function() {
+function addServingToTable() { return function(serving) {
+  $('#tab-history table tbody').append(
+      "<tr><td>" + serving.date + "</td><td>" + serving.type + "</td><td>" + serving.amount + " cl</td></tr>");
+}}
 
+function getServings() {
+  var servings = getUrlAsObservable("http://localhost:8081/api/servings")
+    .Select(function(data) { return $.parseJSON(data).servings })
+    .SelectMany(function(data) { return Rx.Observable.FromArray(data) })
+    .Select(function(data) { return $.parseJSON(data)});
+  servings.Subscribe(addServingToTable());
+}
+
+$(function() {
+  getServings();
 });
