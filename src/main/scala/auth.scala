e35b5536525c0f885802e533677d1af21185f456
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-02 00:32:36 +0200

    use real users repository

--- src/main/scala/auth.scala
+++ src/main/scala/auth.scala
@@ -5,17 +5,15 @@ import org.scalatra.CookieSupport
 import org.scalatra.ScalatraServlet
 import org.scalatra.auth.ScentryAuthStore
 
-trait AuthenticationSupport extends ScentrySupport[User] with FlashMapSupport with CookieSupport { self: ScalatraKernel =>
+trait AuthenticationSupport extends ScentrySupport[User] with FlashMapSupport with CookieSupport { self: BoozementServlet =>
   protected val scentryConfig = (new ScentryConfig {}).asInstanceOf[ScentryConfiguration]
-  
-  override protected def registerAuthStrategies = scentry.registerStrategy('SessionCookie, app => new CookieSessionStrategy(app))
-  //protected def fromSession = { case id: String => Users.getUser(id.toInt) }
-  protected def fromSession = { case id: String => User(Some(1), "jussi.vesala@iki.fi", "foobar") }
+  override protected def registerAuthStrategies = 
+    scentry.registerStrategy('SessionCookie, app => new CookieSessionStrategy(app, database))
+  protected def fromSession = { case id: String => database.user(id.toInt).get }
   protected def toSession = { case usr: User => usr.id.getOrElse("").toString }
-
+ 
   def auth = {
     scentry.authenticate('SessionCookie) 
-    println("user?" + user)  
     user match {
       case user: User => 
       case _ => halt(401, "Unauthorized")
@@ -23,10 +21,10 @@ trait AuthenticationSupport extends ScentrySupport[User] with FlashMapSupport wi
   }
 }
 
-class CookieSessionStrategy(protected val app: ScalatraKernelProxy) extends ScentryStrategy[User] {
+class CookieSessionStrategy(protected val app: ScalatraKernelProxy, val database: DB) extends ScentryStrategy[User] {
   override def authenticate = {
     app.cookies.get("userid") match { 
-      case userid: Some[String] => Some(User(Some(userid.get.toInt), "jussi.vesala@iki.fi2", "foobar2"))
+      case id: Some[String] => database.user(id.get.toInt)
       case _ => None
     }
   }
@@ -35,6 +33,6 @@ class CookieSessionStrategy(protected val app: ScalatraKernelProxy) extends Scen
 trait AutoLogger extends CookieSupport { self: ScalatraServlet =>
   before {
     println("Setting cookie...")
-    //cookies.set("userid", "1")
+    cookies.set("userid", "1")
   }
 }
--- src/test/scala/servletspec.scala
+++ src/test/scala/servletspec.scala
@@ -17,6 +17,8 @@ class ServletSpec extends ScalatraFunSuite with ShouldMatchers with EasyMockSuga
     expecting {
       database.insertServing(new DateTime(2010, 1, 20, 14, 45, 0, 0), "Siideri", 50).andReturn(1)
       lastCall.times(1)
+      database.user(1).andReturn(Some(User(Some(1), "test@test.com", "foobar")))
+      lastCall.times(1)
     }
     whenExecuting(database) {
       post("/insert?date=20.01.2010&time=14:45&type=Siideri&amount=50"){
@@ -30,6 +32,8 @@ class ServletSpec extends ScalatraFunSuite with ShouldMatchers with EasyMockSuga
     expecting {
       database.deleteServing(Some(1)).andReturn(1)
       lastCall.times(1)
+      database.user(1).andReturn(Some(User(Some(1), "test@test.com", "foobar")))
+      lastCall.times(1)      
     }
     whenExecuting(database) {
       post("/delete?id=1"){
@@ -43,6 +47,8 @@ class ServletSpec extends ScalatraFunSuite with ShouldMatchers with EasyMockSuga
     expecting {
       database.servings.andReturn(List(Serving(Some(1), DateTime.now, "Olut", 33)))
       lastCall.times(1)
+      database.user(1).andReturn(Some(User(Some(1), "test@test.com", "foobar")))
+      lastCall.times(1)      
     }
     whenExecuting(database) {
       get("/servings"){
