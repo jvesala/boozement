271f6881ba1a248308b2c9ad520cb2f667e205e0
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-06 22:14:13 +0200

    add one more

--- db/data.sql
+++ db/data.sql
@@ -3,7 +3,7 @@ use boozement;
 #insert into users values(1, "jussi.vesala@iki.fi", "$2a$12$6NGXXN3gneDXR7YBv7cO6ezZraBcn14lrIqcQmydvK.ksMRIfPd9W")
 
 INSERT INTO `servings` VALUES (1, 1, '2010-12-31 19:03:00','Kapuziner Weissbier',50), 
-  (2, 1, '2010-12-30 19:0 0:00','Aulanger jouluolut',33),
+  (2, 1, '2010-12-30 19:00:00','Aulanger jouluolut',33),
   (3, 1, '2010-12-30 14:00:00','Corona',33),
   (4, 1, '2010-12-30 14:30:00','Koff Porter',33),
   (5, 1, '2010-12-30 17:30:00','Koff III',33),
@@ -12,4 +12,5 @@ INSERT INTO `servings` VALUES (1, 1, '2010-12-31 19:03:00','Kapuziner Weissbier'
   (8, 1, '2010-12-31 22:01:00','Shampanja',10),
   (9, 1, '2010-12-31 22:30:00','Corona',33),
   (10, 1, '2011-01-01 16:12:00','Pink-kuohuviini',18),
-  (11, 1, '2011-01-01 18:23:00','Pink-kuohuviini',25);
+  (11, 1, '2011-01-01 18:23:00','Pink-kuohuviini',25),
+  (12, 1, '2011-01-06 19:30', 'Chianti', 18);
