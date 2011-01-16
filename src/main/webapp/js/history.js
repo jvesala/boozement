242edf1f58c9cd11f8d85750f7615068fb24e26f
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-16 18:06:18 +0200

    search first working version

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -1,13 +1,10 @@
 var tBody = $('#tab-history table tbody')
 function clearServings() { tBody.empty("") }
 function addServingToTable(s) { tBody.append('<tr><td class="date">' + s.date + '</td><td class="servingType">' + s.type + '</td><td class="amount">' + s.amount + " cl</td></tr>") }
-function addServingsToTable(servings) {
-  $.each(servings, function(i, s) {
-    addServingToTable($.parseJSON(s))
-  })
-}
+function addServingsToTable(servings) { $.each(servings, function(i, s) { addServingToTable($.parseJSON(s)) }) }
 
 function query(term, page) {
+  showBusy()  
   var params = { query:escape(term), page:page }
   var servings = $.ajaxAsObservable({ url: "api/servings", data: params}).Publish()
   handleUnauthorized(servings)
@@ -27,38 +24,27 @@ function servings() {
       .Scan(0, function(current, x) { return current+1 })
       .SelectMany(function(page) { return query(term, page) })
   }
-  var first = input.Select(function(term) { return query(term, 0) }).Switch()
-  var initialResults = query("", 0)
-  var first = initialResults.Merge(input.Select(function(term) { return query(term, 0) }).Switch())
-  var more = input.Select(paging).Switch()
+  var first = input.Select(function(term) { return query(term, 0) }).Switch().Publish()
+  var more = input.Select(paging).Switch().Publish()
   var scrollBottom = $('table tbody').toObservable('scroll')
     .Select(isScrolledToBottom)
     .DistinctUntilChanged()
     .Where(function(inBottom) { return inBottom })
-  
   first.Subscribe(function(servings) { clearServings(); addServingsToTable(servings) })
-  first.Subscribe(function(x) { hideBusy() });
-  more.Subscribe(function(servings) { addServingsToTable(serving) })
-  more.Subscribe(function(x) { hideBusy() });
-
-
+  first.Subscribe(function(x) { hideBusy() })  
+  more.Subscribe(function(servings) { addServingsToTable(servings) })
+  more.Subscribe(function(x) { hideBusy() })
+  first.Connect()
+  more.Connect()
+  $('#search').keyup()
 }
 
-
-
-$(function() {
-  showBusy();
-  servings();
-  updateLoggedIn();  
-});
-
-
-
 function isScrolledToBottom(e) {
-  var scrollTolerance = 100
+  var scrollTolerance = 10
   return e.target.scrollTop + e.target.offsetHeight + scrollTolerance > e.target.scrollHeight
 }
 
-
-
-
+$(function() {
+  servings()
+  updateLoggedIn()
+});
