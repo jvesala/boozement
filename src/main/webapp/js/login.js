fe748440131906b444f7861b03fc2cb40aeddc9e
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-21 07:10:55 +0200

    refactor

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -4,10 +4,11 @@ function doLogin() {
   preSubmit()
   var login = $.postAsObservable("api/login", loginParams()).Publish()
   login.Subscribe(skip, function(error) {
-    if(error.xmlHttpRequest.status == "401") { updateResult("Väärä kirjautumistunnus tai salasana."); resetSubmitStatus() }
-    else if(error.xmlHttpRequest.status != "200") { updateResult("Virhetilanne kirjautumisessa. Yritä uudelleen."); resetSubmitStatus() }
+    if(error.xmlHttpRequest.status == "401") { updateResult("Väärä kirjautumistunnus tai salasana.") }
+    else if(error.xmlHttpRequest.status != "200") { updateResult("Virhetilanne kirjautumisessa. Yritä uudelleen.") }
+    resetSubmitStatus()
   }, skip)
-  var loginContent = login.Select(function(d) { return d.data; }).Catch(Rx.Observable.Never()).Publish()
+  var loginContent = login.Select(function(d) { return d.data }).Catch(Rx.Observable.Never()).Publish()
   loginContent.Subscribe(setPageContent)
   loginContent.Subscribe(function(x) { showTabHeaders(); updateLoggedIn() })
   login.Connect()
