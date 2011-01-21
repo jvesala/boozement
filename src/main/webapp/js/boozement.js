f8e51c2d77a0bd7ed803eb999dcd0c6c810af456
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-21 16:29:56 +0200

    change submit to button outside the form

--- src/main/webapp/js/boozement.js
+++ src/main/webapp/js/boozement.js
@@ -46,8 +46,8 @@ function showBusy() { $('.busy').show() }
 function hideBusy() { $('.busy').hide() }
 function showTabHeaders() { $('.tab-header').show() }
 function hideTabHeaders() { $('.tab-header').hide() }
-function enableSubmitButton() { $('input[type=submit]').removeAttr("disabled") }
-function disableSubmitButton() { $('input[type=submit]').attr("disabled", "disabled") }
+function enableSubmitButton() { $('button').removeAttr("disabled") }
+function disableSubmitButton() { $('button').attr("disabled", "disabled") }
 function preSubmit() { disableSubmitButton(); showBusy() }
 function resetSubmitStatus() { hideBusy(); enableSubmitButton() }
 
