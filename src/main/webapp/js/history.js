c09313278023ddfd7706924f503d3de7acfe77f4
Jussi Vesala <jussi.vesala@iki.fi>
2011-01-23 23:52:25 +0200

    add inplace edit to history page, todo serverside

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -5,11 +5,32 @@ function showCount(count) { $('#count').show(); $('#count span').html(count) }
 function hideCount() { $('#count').hide() }
 function clearServings() { tBody.empty("") }
 function clearSearch() { search.val("").keyup() }
-function addServingToTable(s) { 
+
+function updateField(target) {
+  var currentValue = target.val()
+  var parent = target.parent()
+  parent.html(currentValue)
+}
+function eventTarget(e) { return $(e.target) }
+function enterPressed(e) { return e.keyCode == 13}
+function openEdit(target) {
+  var inputName = target.parent().attr("class") + '-' +  target.attr("class")
+  var originalValue = target.html()
+  var input = $('<input type="text" name="' + inputName + '" value="' + originalValue + '" />')
+  input.toObservable('blur').Select(eventTarget).Subscribe(updateField)
+  input.toObservable('keyup').Where(enterPressed).Select(eventTarget).Subscribe(updateField)
+  target.empty().append(input)
+  input.focus()
+}
+
+function addServingToTable(s) {
   var date = $('<td class="date">' + s.date + '</td>')
+  date.toObservable('click').Select(eventTarget).Subscribe(openEdit)
   var type = $('<td class="servingType">' + s.type + '</td>')
+  type.toObservable('click').Select(eventTarget).Subscribe(openEdit)
   var amount = $('<td class="amount">' + s.amount + ' cl</td>')
-  var row = $('<tr></tr>').append(date).append(type).append(amount)
+  amount.toObservable('click').Select(eventTarget).Subscribe(openEdit)
+  var row = $('<tr class="' + s.id + '"></tr>').append(date).append(type).append(amount)
   tBody.append(row)
 }
 function addServingsToTable(servings) { $.each(servings, function(i, s) { addServingToTable(s) }) }
