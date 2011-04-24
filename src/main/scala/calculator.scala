45cc3e5f08c2a3b0a373f2a4b77c8efb109abbe2
Jussi Vesala <jussi.vesala@iki.fi>
2011-04-24 20:17:02 +0300

    calculate bac with one serving, todo endtime...

--- src/main/scala/calculator.scala
+++ src/main/scala/calculator.scala
@@ -2,8 +2,26 @@ import org.scala_tools.time.Imports._
 
 class Calculator {
   
-  def calculateBac(user: User, now: DateTime, servings: List[Serving]): Double = {
-    0
+  def alcoholInGramsInServing(serving: Serving) = 12D
+  
+  def burnRate(weight: Double) = 0.1 * weight / (3600 * 1000)
+  
+  def remainingAmount(weight: Double, startGrams: Double, start: DateTime, end: DateTime) =
+    startGrams - burnRate(weight) * (end.millis - start.millis)
+  
+  def genderFactor(gender: String) = if (gender == "M") 0.75 else 0.66
+  
+  def gramsToBac(user: User, grams: Double) = grams / (genderFactor(user.gender) * user.weight) * 1000
+  
+  def calculateBacHistory(user: User, now: DateTime, servings: List[Serving]): List[(Double, DateTime)] = {
+    val initTime = if(servings.size > 0) servings.head.date else now
+    val init = (0D, initTime)
+    val gramHistory = servings.scanLeft(init)( (last, next) => {
+      (remainingAmount(user.weight, last._1, last._2, next.date) + alcoholInGramsInServing(next), next.date)
+    })    
+    gramHistory.map( x => { (gramsToBac(user, x._1), x._2) })
   }
   
 }
+
+object Calculator extends Calculator
--- src/test/scala/calculatorspec.scala
+++ src/test/scala/calculatorspec.scala
@@ -2,18 +2,22 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.FunSuite
 import org.scala_tools.time.Imports._
- 
-trait TestEnv { self : BoozementDatabase => override def dbUrl = "jdbc:mysql://127.0.0.1:3306/boozement_test?user=boozement&password=boozement" }
- 
+  
 class CalculatorSpec extends FunSuite with BeforeAndAfterAll with BeforeAndAfterEach {
   val calculator = new Calculator
   
   def user = User(Some(1), "test@user.com", "$2a$12$6NGXXN3gneDXR7YBv7cO6ezZraBcn14lrIqcQmydvK.ksMRIfPd9W", "M", 75000)
-  
+    
   test("should return zero when no drinks consumed") {
-    assert(calculator.calculateBac(user, new DateTime(), Nil) == 0)
+    val bacHistory = calculator.calculateBacHistory(user, new DateTime(), Nil)
+    assert(bacHistory.head._1 == 0)
   }
   
-
+  test("should return 0.22 for M/75kg immediately after beer has been consumed") {
+    val time = new DateTime()
+    val serving = new Serving(None, user.id, time, "Olut", 33)
+    val bacHistory = calculator.calculateBacHistory(user, time, List(serving))
+    assert(bacHistory.last._1 == 0.21333333333333332)    
+  }
 
 }
