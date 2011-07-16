977003212502b674126035a8cd965b83bb213e91
Jussi Vesala <jussi.vesala@iki.fi>
2011-07-16 13:29:13 +0300

    test female factor

--- src/main/scala/calculator.scala
+++ src/main/scala/calculator.scala
@@ -1,24 +1,26 @@
+import scala.math._
 import org.scala_tools.time.Imports._
 
 class Calculator {
-  
-  def alcoholInGramsInServing(serving: Serving) = 12D
-  
+  def alcoholInGramsInServing(serving: Serving) = if (serving.amount > 0) 12D else 0D
+
   def burnRate(weight: Double) = 0.1 * weight / (3600 * 1000)
-  
-  def remainingAmount(weight: Double, startGrams: Double, start: DateTime, end: DateTime) =
-    startGrams - burnRate(weight) * (end.millis - start.millis)
-  
+
+  def remainingAmount(weight: Double, startGrams: Double, start: DateTime, end: DateTime) = {
+    max(startGrams - burnRate(weight) * (end.millis - start.millis), 0)
+  }
+
   def genderFactor(gender: String) = if (gender == "M") 0.75 else 0.66
-  
+
   def gramsToBac(user: User, grams: Double) = grams / (genderFactor(user.gender) * user.weight) * 1000
-  
+
   def calculateBacHistory(user: User, now: DateTime, servings: List[Serving]): List[(Double, DateTime)] = {
-    val initTime = if(servings.size > 0) servings.head.date else now
-    val init = (0D, initTime)
-    val gramHistory = servings.scanLeft(init)( (last, next) => {
+    val startTime = if(servings.size > 0) servings.head.date else now
+    val start = (0D, startTime)
+    val end = Serving(None, None, now, "", 0)
+    val gramHistory = (servings ++ List(end)).scanLeft(start)( (last, next) => {
       (remainingAmount(user.weight, last._1, last._2, next.date) + alcoholInGramsInServing(next), next.date)
-    })    
+    })
     gramHistory.map( x => { (gramsToBac(user, x._1), x._2) })
   }
   
--- src/test/scala/calculatorspec.scala
+++ src/test/scala/calculatorspec.scala
@@ -5,19 +5,27 @@ import org.scala_tools.time.Imports._
   
 class CalculatorSpec extends FunSuite with BeforeAndAfterAll with BeforeAndAfterEach {
   val calculator = new Calculator
-  
-  def user = User(Some(1), "test@user.com", "$2a$12$6NGXXN3gneDXR7YBv7cO6ezZraBcn14lrIqcQmydvK.ksMRIfPd9W", "M", 75000)
-    
+
+  def user = User(None, "", "", "M", 75000)
+  def user2 = User(None, "", "", "F", 69000)
+
+  def now = new DateTime
+
   test("should return zero when no drinks consumed") {
     val bacHistory = calculator.calculateBacHistory(user, new DateTime(), Nil)
     assert(bacHistory.head._1 == 0)
   }
   
   test("should return 0.22 for M/75kg immediately after beer has been consumed") {
-    val time = new DateTime()
-    val serving = new Serving(None, user.id, time, "Olut", 33)
-    val bacHistory = calculator.calculateBacHistory(user, time, List(serving))
+    val serving = new Serving(None, user.id, now, "Olut", 33)
+    val bacHistory = calculator.calculateBacHistory(user, now, List(serving))
     assert(bacHistory.last._1 == 0.21333333333333332)    
+  }
+
+  test("should return 0.26 for F/69kg immediately after beer has been consumed") {
+    val serving = new Serving(None, user2.id, now, "Olut", 33)
+    val bacHistory = calculator.calculateBacHistory(user2, now, List(serving))
+    assert(bacHistory.last._1 == 0.26350461133069825)    
   }
 
 }
