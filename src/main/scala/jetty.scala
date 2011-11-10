aef7d534d1471f60c18478745107d0fddc4c12d3
Jussi Vesala <jussi.vesala@iki.fi>
2011-11-10 18:24:03 +0200

    use assembly plugin to generate runnable jar with embedded jetty (took 6 hours)

--- src/main/scala/jetty.scala
+++ src/main/scala/jetty.scala
@@ -1,26 +1,25 @@
 import org.mortbay.jetty.Server
+import org.mortbay.jetty.Handler
 import org.mortbay.jetty.servlet.{Context, ServletHolder, DefaultServlet}
-import org.mortbay.jetty.handler.{ResourceHandler, DefaultHandler, HandlerList, ContextHandler, HandlerCollection}
+import org.mortbay.jetty.handler._
 
+class Jetty
 
 object Jetty {
   def main(args: Array[String]) {
     val server = new Server(if(args.length > 0) args(0).toInt else 8080)
 
-    val handler = new ResourceHandler
-    handler.setWelcomeFiles(Array("boozement.html"))
-    handler.setResourceBase("src/main/webapp")
-    
-    val handlers = new HandlerList
-    handlers.addHandler(handler)
-
-    val collection = new HandlerCollection
-    collection.addHandler(handlers)
-    server.setHandler(collection)
-
     val servlet = new Context(server, "/api", Context.SESSIONS)
     servlet.addServlet(new ServletHolder(new BoozementServlet), "/*")
 
+    val static = new Context(server, "/", Context.SESSIONS)
+    val base = classOf[Jetty].getClassLoader().getResource("static").toExternalForm
+    static.setResourceBase(base)
+    val staticHolder = new ServletHolder
+    staticHolder.setInitParameter("dirAllowed", "false")
+    staticHolder.setServlet(new DefaultServlet)
+    static.addServlet(staticHolder, "/*")
+    
     server.start
     server.join
   }
