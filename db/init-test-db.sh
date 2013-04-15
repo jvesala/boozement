e57f701c93785fc9d66bce1e2d908fd1af3fd86d
Jussi Vesala <jussi.vesala@iki.fi>
2013-04-15 14:40:43 +0300

    fix init script header

--- db/init-test-db.sh
+++ db/init-test-db.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 mysql -u root -h 127.0.0.1 -e "drop database boozement_test;"
 mysql -u root -h 127.0.0.1 -e "create database boozement_test;"
 mysql -u root -h 127.0.0.1 boozement_test < testdata.sql
