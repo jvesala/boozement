78bb76bc8b4554a077892ba6881e14e2185fd24d
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-09 21:27:41 -0400

    add something

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Menu} from "./components/Menu";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
+      <Menu />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +23,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
