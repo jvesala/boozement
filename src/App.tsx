6211b72396a926ab72279eb394a0e886f04f2769
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-10 21:24:53 -0400

    use prettier, todo jsons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Menu} from "./components/Menu";
+import { Menu } from './components/Menu';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <Menu />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 };
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,21 +2,37 @@ import React from 'react';
 import './Menu.css';
 
 export const Menu = () => {
-  return (
-    <div className="Menu">
-      <header className="Menu-header">
-          <div className="logo">
-            <h1>Boozement</h1>
-          </div>
-          <nav id="nav">
-            <ul>
-              <li className="tab-header"><a href="#insert" id="tab-header-insert">Syötä</a></li>
-              <li className="tab-header"><a href="#active" id="tab-header-active">Nyt juonut</a></li>
-              <li className="tab-header"><a href="#history" id="tab-header-history">Historia</a></li>
-              <li className="tab-header"><a href="#userdata" id="tab-header-userdata">Omat tiedot</a></li>
-            </ul>
-          </nav>
-        </header>
-    </div>
-  );
+    return (
+        <div className="Menu">
+            <header className="Menu-header">
+                <div className="logo">
+                    <h1>Boozement</h1>
+                </div>
+                <nav id="nav">
+                    <ul>
+                        <li className="tab-header">
+                            <a href="#insert" id="tab-header-insert">
+                                Syötä
+                            </a>
+                        </li>
+                        <li className="tab-header">
+                            <a href="#active" id="tab-header-active">
+                                Nyt juonut
+                            </a>
+                        </li>
+                        <li className="tab-header">
+                            <a href="#history" id="tab-header-history">
+                                Historia
+                            </a>
+                        </li>
+                        <li className="tab-header">
+                            <a href="#userdata" id="tab-header-userdata">
+                                Omat tiedot
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
 };
