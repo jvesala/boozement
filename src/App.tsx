cd473f3aea6114295940ff30381766f3cea467e9
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-11 20:47:37 -0400

    add login element

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Menu } from './components/Menu';
+import { Login } from './components/Login';
 
 export const App = () => {
     return (
         <div className="App">
             <Menu />
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Login />
         </div>
     );
 };
