2a3a4bda2ffc71d0188ca6ba3ee476a7ce7c06fa
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-15 20:38:26 -0400

    add server and proxy it to one command

---
+++ src/server/server.ts
@@ -0,0 +1,27 @@
+//import express from 'express';
+// import bodyParser from 'body-parser';
+
+const express = require('express');
+import { Request, Response } from 'express'
+
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (_req: Request, res: Response) => {
+
+    res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
