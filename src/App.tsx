17a50005bc230d4d4caff6000eb41918ad869f1e
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-22 21:23:48 -0400

    fix login redirect

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Userdata } from './features/userdata/Userdata';
 import { selectUser } from './features/login/loginSlice';
 import { useSelector } from 'react-redux';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const App = () => {
     const user = useSelector(selectUser);
@@ -19,10 +19,19 @@ export const App = () => {
             <Menu />
             {!user ? <Login /> : ''}
 
-            {user ? <Route path="/insert" component={Insert} /> : ''}
-            {user ? <Route path="/active" component={Active} /> : ''}
-            {user ? <Route path="/history" component={History} /> : ''}
-            {user ? <Route path="/userdata" component={Userdata} /> : ''}
+            {user ? (
+                <div>
+                    <Switch>
+                        <Redirect exact from="/" to="/insert" />
+                        <Route path="/insert" component={Insert} />
+                        <Route path="/active" component={Active} />
+                        <Route path="/history" component={History} />
+                        <Route path="/userdata" component={Userdata} />
+                    </Switch>
+                </div>
+            ) : (
+                ''
+            )}
         </div>
     );
 };
