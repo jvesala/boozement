b8407be15848945ebaab99de8767d1fccd9c6070
Jussi Vesala <jussi.vesala@reaktor.fi>
2020-03-25 21:18:11 -0400

    add indices and dump insert

--- db/schema.sql
+++ db/schema.sql
@@ -10,13 +10,17 @@ CREATE TABLE users
     weight   INT          NOT NULL
 );
 
+CREATE INDEX users_email_idx ON users USING btree (email);
+
 DROP TABLE IF EXISTS servings;
 CREATE TABLE servings
 (
-    id     SERIAL PRIMARY KEY,
-    userid INTEGER REFERENCES users(id),
-    date   TIMESTAMP WITH TIME ZONE,
-    type   VARCHAR(254) NOT NULL,
-    amount INT          NOT NULL,
-    units  NUMERIC      NOT NULL
+    id      SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users (id),
+    date    TIMESTAMP WITH TIME ZONE,
+    type    VARCHAR(254) NOT NULL,
+    amount  INT          NOT NULL,
+    units   NUMERIC      NOT NULL
 );
+
+CREATE INDEX servings_user_id_idx ON servings USING btree (user_id);
