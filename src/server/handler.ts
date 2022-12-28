e7d88dcea4944c1deb8afb50769d8776a24a0f7b
Jussi Vesala <jussi.vesala@reaktor.com>
2022-12-28 14:48:17 -0500

    update some deps

--- src/server/handler.ts
+++ src/server/handler.ts
@@ -18,7 +18,7 @@ export const tryCatchHandler = async (
     try {
         console.log(`${req.method} ${req.url}`, req.body);
         fn();
-    } catch (e) {
+    } catch (e: any) {
         console.log('API error', e);
         res.status(500).json(JSON.stringify({ msg: e.message }));
     }
