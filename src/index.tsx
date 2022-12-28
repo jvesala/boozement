a6b7e083abe4c97895f5d060ed557b3897839481
Jussi Vesala <jussi.vesala@reaktor.com>
2022-12-28 14:48:54 -0500

    remove unused file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { configureStoreWithState } from './app/store';
 
@@ -25,8 +24,3 @@ ReactDOM.hydrate(
     </Provider>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
