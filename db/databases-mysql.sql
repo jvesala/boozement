3f43fc75d2696e46a9d04cf1f3ae3c49ebc51566
Jussi Vesala <jussi.vesala@reaktor.com>
2022-12-29 16:14:46 -0500

    remove mysql stuff

--- db/databases-mysql.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists boozement;
-create database boozement;
-grant all on boozement.* to 'boozement'@'%' identified by 'boozement';
-grant all on boozement.* to 'boozement'@'localhost' identified by 'boozement';
-
-drop database if exists boozement_test;
-create database boozement_test;
-grant all on boozement_test.* to 'boozement'@'%' identified by 'boozement';
-grant all on boozement_test.* to 'boozement'@'localhost' identified by 'boozement';
--- db/init-test-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mysql -u root -h 127.0.0.1 -e "drop database boozement_test;"
-mysql -u root -h 127.0.0.1 -e "create database boozement_test;"
-mysql -u root -h 127.0.0.1 boozement_test < testdata.sql
-mysql -u root -h 127.0.0.1 boozement_test -e "rename table servings to servings_template"
-mysql -u root -h 127.0.0.1 boozement_test -e "rename table users to users_template"
