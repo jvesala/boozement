815320599597eb111093f12ae6a2814996b7f6d7
Jussi Vesala <jussi.vesala@reaktor.com>
2022-12-29 14:12:20 -0500

    fix default navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Userdata } from './features/userdata/Userdata';
 import { selectUser } from './features/login/loginSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { whoAmIAsync } from './features/whoami/whoAmISlice';
 
@@ -33,6 +33,7 @@ export const App = () => {
             <Route path="/active" element={<Active />} />
             <Route path="/history" element={<History />} />
             <Route path="/userdata" element={<Userdata />} />
+            <Route path="*" element={<Navigate to="/insert"/>} />
           </Routes>
         ) : (
           <Login />
